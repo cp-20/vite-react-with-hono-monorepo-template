7038c2dca06c49e04c1492e879e6e4534d7cd690
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2024-05-17 10:21:31 +0900

    monorepoの設定をちょっとした

--- packages/app/.eslintrc.cjs
+++ packages/app/.eslintrc.cjs
@@ -2,10 +2,19 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
+    'plugin:@typescript-eslint/strict-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
@@ -15,4 +24,4 @@ module.exports = {
       { allowConstantExport: true },
     ],
   },
-}
+};
---
+++ packages/app/README.md
@@ -0,0 +1,2 @@
+# React with Hono Monorepo Template
+
